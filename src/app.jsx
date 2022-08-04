71a84f68b6de7a3ba86e92b952718e1ae7701310
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-08-04 19:45:48 +0530

    add toastify

--- src/app.jsx
+++ src/app.jsx
@@ -4,21 +4,25 @@ import Footer from "./components/footer";
 import Body from "./components/body";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Result from "./components/result";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const response = axios.create;
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Body />} />
-          <Route path="/weather" element={<Result />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Body />} />
+            <Route path="/weather" element={<Result />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 export default App;
